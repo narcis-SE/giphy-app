527d9645b32da63be2a925250d9511c0ff79f5e5
narcis <narcissprecic@gmail.com>
2022-03-18 13:01:24 -0400

    button to display more + styles

--- src/components/ResultsList.tsx
+++ src/components/ResultsList.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 import { Data } from "../models/Search";
 import { Result } from "./Results";
 
@@ -8,13 +10,21 @@ export interface Props{
 }
 
 export const ResultsList = ({gifs}: Props)=>{
+    const [condition, setCondition] = useState<boolean>(true)
+
+
     return(
         <div className="ResultsList">
             <h1 >Results: </h1>
 
             <div className="container">
                 <div className="resultsList">
-                    {gifs.map((gif,i) => <Result key={i} gif={gif}/>)}
+                    {
+                        condition ? <div className="resultsList">{gifs.slice(0,3).map((gif,i) => <Result key={i} gif={gif}/>)} </div> : <div className="resultsList">{gifs.map((gif,i) => <Result key={i} gif={gif}/>)} </div>
+                    }
+                    
+                    {/* {gifs.slice(0,3).map((gif,i) => <Result key={i} gif={gif}/>)} */}
+                        <Button onClick={() => setCondition(false)}> Display All Results</Button>
                 </div>
             </div>
 
